73a1bfdf81a98ddf6e1d5f566a454f3be7c4358c
Greenhoody <1mk2mk3mk@gmail.com>
2022-01-28 02:14:19 +0100

    dodanie WebApp

---
+++ FlotaPojazdów.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FlotaPojazdów.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
